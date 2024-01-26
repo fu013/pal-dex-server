7b05a6829f5f6775713a37e5a5826b6be3a7b394
fu013 <fu017@naver.com>
2024-01-26 15:28:54 +0900

    feat: init

---
+++ src/main/java/dir/group/paldexserver/PalDexServerApplication.java
@@ -0,0 +1,13 @@
+package dir.group.paldexserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalDexServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalDexServerApplication.class, args);
+	}
+
+}
