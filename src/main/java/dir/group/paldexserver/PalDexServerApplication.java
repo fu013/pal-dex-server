da8c70b4a26cf0840003c894f5b80bfd37687302
fu013 <fu017@naver.com>
2024-01-30 15:25:31 +0900

    feat: spring-boot 초기 설정

--- src/main/java/dir/group/paldexserver/PalDexServerApplication.java
+++ src/main/java/dir/group/paldexserver/PalDexServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PalDexServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PalDexServerApplication.class, args);
 	}
-
 }
---
+++ src/main/java/dir/group/paldexserver/controller/AppController.java
@@ -0,0 +1,13 @@
+package dir.group.paldexserver.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class AppController {
+    @GetMapping("/")
+    public String app() throws IOException {
+        return "shop";
+    }
+}
