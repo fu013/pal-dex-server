9fcb1c326c890fdf61bcd5d110929b9ac87701c0
fu013 <fu017@naver.com>
2024-02-29 10:34:54 +0900

    fix: post fetch join sql update

--- src/main/java/dir/group/paldexserver/repository/PostRepository.java
+++ src/main/java/dir/group/paldexserver/repository/PostRepository.java
@@ -13,15 +13,21 @@ public interface PostRepository extends JpaRepository<PostEntity, Long> {
     @Query(value = "SELECT p.pk AS pk, p.title AS title, " +
             "p.html AS html, p.markdown AS markdown, p" +
             ".description AS description, p.tags AS tags," +
-            " p.is_public AS isPublic, p.is_review AS isReview, p.comment AS comment, f.path AS path FROM post p INNER JOIN file f ON p.pk = f.tpk AND f.t_name = 'post' AND f.is_thumb = '1'", nativeQuery = true)
+            " p.is_public AS isPublic, p.is_review AS isReview, p.comment AS comment, f.path AS path " +
+            "FROM post p LEFT JOIN file f ON p.pk = f.tpk AND f.t_name = 'post' AND f.is_thumb = '1'",
+            nativeQuery = true)
     List<PostWithFilePathProjection> findAllPosts();
 
     @Query(value = "SELECT p.pk AS pk, p.title AS title, " +
             "p.html AS html, p.markdown AS markdown, p" +
             ".description AS description, p.tags AS tags," +
             " p.is_public AS isPublic, p.is_review AS " +
-            "isReview, p.comment AS comment, f.path AS path FROM post p LEFT JOIN file f ON p.pk = f.tpk AND f.t_name = 'post' AND f.is_thumb = '1' WHERE FIND_IN_SET(:tag, REPLACE(p.tags, ', ', ',')) > 0", nativeQuery = true)
+            "isReview, p.comment AS comment, f.path AS path " +
+            "FROM post p LEFT JOIN file f ON p.pk = f.tpk AND f.t_name = 'post' AND f.is_thumb = '1' " +
+            "WHERE FIND_IN_SET(:tag, REPLACE(p.tags, ', ', ',')) > 0",
+            nativeQuery = true)
     List<PostWithFilePathProjection> findByTag(@Param("tag") String tag);
+
 
     Optional<PostEntity> findByTitle(String title);
 }
